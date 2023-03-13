89abaeecd7e1d16ee19e706f3cf205c405106b3c
H1net <ben@freshdevs.com>
2023-03-13 17:03:19 +0000

    feat: add query for key requests and more loading pages

--- app/property/key/page.tsx
+++ app/property/key/page.tsx
@@ -1,5 +1,27 @@
-export default function KeysPage() {
+import { supabase } from "@/lib/supabase"
+import Link from "next/link";
+
+export default async function KeysPage() {
+  const { data } = await supabase.from('property_keys').select('*, propertys (*)');
+  // console.log(data)
   return (
-    <div>Keys List</div>
+    <div>
+      <h1>Keys List</h1>
+      <div>
+      {/* <select>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select> */}
+      </div>
+      <div>
+        {data?.map((propertyKey) => (
+          <Link href={`/property/key/${propertyKey.id}`} key={propertyKey.id}>
+            <p>Tag number: {propertyKey?.tag_number}</p>
+            <p>Address: {propertyKey?.propertys?.address}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
