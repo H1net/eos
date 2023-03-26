2f3dd73241c6531eec590c139887de7ef98cc5d4
H1net <ben@freshdevs.com>
2023-03-26 23:22:43 +0000

    fix: fix types for property keys

--- app/property/key/page.tsx
+++ app/property/key/page.tsx
@@ -1,10 +1,30 @@
 import { supabaseCreateForServer } from "@/lib/supabase-server";
 import Link from "next/link";
+import { Database } from "@/lib/database.types";
+import LoadingNotifier from "@/components/LoadingNotifier";
+type PropertyKey = Database['public']['Tables']['property_keys']['Row']
+type Property = Database['public']['Tables']['propertys']['Row']
+
+export const revalidate = 0
+
+async function getPropertyKeys() {
+  const supabase = supabaseCreateForServer()
+  return await supabase.from('property_keys').select('*, propertys (*)')
+}
+
+type PropertyKeysResponse = Awaited<ReturnType<typeof getPropertyKeys>>
+type PropertyKeysResponseSuccess = PropertyKeysResponse['data']
+type PropertyKeysResponseError = PropertyKeysResponse['error']
+
 
 export default async function KeysPage() {
-  const supabase = supabaseCreateForServer()
-  const { data } = await supabase.from('property_keys').select('*, propertys (*)');
+  const propertyKeysResponse: PropertyKeysResponse = await getPropertyKeys()
   // console.log(data)
+
+  if (!Array.isArray(propertyKeysResponse.data)) {
+    return <LoadingNotifier />
+  }
+
   return (
     <div>
       <h1>Keys List</h1>
@@ -16,9 +36,10 @@ export default async function KeysPage() {
       </select> */}
       </div>
       <div>
-        {data?.map((propertyKey) => (
+        {propertyKeysResponse.data?.map((propertyKey) => (
           <Link href={`/property/key/${propertyKey.id}`} key={propertyKey.id}>
             <p>Tag number: {propertyKey?.tag_number}</p>
+            {/* @ts-expect-error */}
             <p>Address: {propertyKey?.propertys?.address}</p>
           </Link>
         ))}
