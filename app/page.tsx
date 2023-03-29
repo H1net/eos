952b8d732a0988d0c7abfd7bcac0d6b36fc64b2a
H1net <ben@freshdevs.com>
2023-03-29 14:09:45 +0100

    feat: add auth check on home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,55 @@
-import PinBoard from '@/components/PinBoard';
+// 'use client'
 
-export default function HomePage() {
+import PinBoard from '@/components/PinBoard'
+// import Auth from '@/components/Auth'
+// import { useAuth, VIEWS } from '@/components/AuthProvider'
+import LoadingNotifier from '@/components/LoadingNotifier'
+import { supabaseCreateForServer } from '@/lib/supabase-server'
+
+export default async function HomePage() {
+  // const { initial, user, view, signOut } = useAuth()
+  const supabase = supabaseCreateForServer()
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // console.log(user)
+
+  // if (initial) {
+  //   return <LoadingNotifier />
+  // }
+
+  if (session?.user) {
+    return (
+      <main>
+        <section>
+          <article className="card">
+            <div className="card-body">
+              <p>
+                Welcome to <span className="font-bold underline">Eos</span>
+              </p>
+            </div>
+          </article>
+        </section>
+        {/* @ts-expect-error */}
+        <PinBoard />
+      </main>
+    )
+  }
+
   return (
     <main>
       <section>
-        <article className='card'>
-          <div className='card-body'>
-            <p>Welcome to <span className='font-bold underline'>Eos</span></p>
+        <article className="card">
+          <div className="card-body">
+            <p>
+              Welcome to <span className="font-bold underline">Eos</span>
+            </p>
             <p>Please sign in for the menu.</p>
           </div>
         </article>
       </section>
-      {/* @ts-expect-error */}
-      <PinBoard />
     </main>
   )
 }
